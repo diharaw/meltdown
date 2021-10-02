8c6fc64754e81db1675a72a1f226be621fb02064
diharaw <shredhead94@gmail.com>
2021-10-02 09:49:35 +0100

    Basic enemy path finding.

---
+++ LD49/Assets/Scripts/EnemyController.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.InputSystem;
+
+public class EnemyController : MonoBehaviour
+{
+    public Camera m_camera;
+    
+    private NavMeshAgent m_agent;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_agent = GetComponent<NavMeshAgent>();
+        m_agent.SetDestination(new Vector3(0.0f, 0.0f, 0.0f));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
