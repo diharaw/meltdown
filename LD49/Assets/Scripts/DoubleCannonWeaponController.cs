13ffe5f18071a4232866ca1f7abd09a76ff1e364
diharaw <shredhead94@gmail.com>
2021-10-03 09:56:41 +0100

    Cleaned up prefabs.

--- LD49/Assets/Scripts/DoubleCannonWeaponController.cs
+++ LD49/Assets/Scripts/DoubleCannonWeaponController.cs
@@ -2,17 +2,53 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class DoubleCannonWeaponController : MonoBehaviour
+public class DoubleCannonWeaponController : WeaponController
 {
+    public ParticleSystem m_particleSystem2;
+    public Transform m_muzzleTip2;
+
+    private Animator m_animator;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        m_animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+
+    public override void StartFiring()
+    {
+        m_animator.SetBool("isFiring", true);
+    }
+
+    public override void StopFiring()
+    {
+        m_animator.SetBool("isFiring", false);
+    }
+
+    public override void Fire()
+    {
+        m_particleSystem.Play();
+        m_particleSystem2.Play();
+        m_audioSource.Play();
+
+        {
+            GameObject bullet = BulletPool.sharedInstance.GetPooledCannonBullet();
+            bullet.transform.position = m_muzzleTip.position;
+            bullet.transform.rotation = m_muzzleTip.rotation;
+            bullet.GetComponent<Bullet>().m_effectiveLayer = m_effectiveLayer;
+        }
+
+        {
+            GameObject bullet = BulletPool.sharedInstance.GetPooledCannonBullet();
+            bullet.transform.position = m_muzzleTip2.position;
+            bullet.transform.rotation = m_muzzleTip2.rotation;
+            bullet.GetComponent<Bullet>().m_effectiveLayer = m_effectiveLayer;
+        }
     }
 }
